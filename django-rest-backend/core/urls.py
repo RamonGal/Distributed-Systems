b52cbd6aadd9c354a56aacf76861b3e7a40fe6e2
ramon-ubuntu <ramon_rib@hotmail.com>
2022-11-18 08:36:29 -0300

    docs settup

--- django-rest-backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import include, path
-
-# urls
-urlpatterns = [
-    path('api/auth/', include('authentication.urls')),
-    path('admin/', admin.site.urls),
-]
